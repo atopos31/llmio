3193da70f72d466a9d419933ff513f65b7fa7d19
hackerxiao <2945294768@qq.com>
2025-07-19 17:40:53 +0800

    新增 ProviderTestHandler 函数以支持提供者测试功能

--- handler/test.go
+++ handler/test.go
@@ -1,8 +1,9 @@
-package service
+package handler
 
 import "github.com/gin-gonic/gin"
 
-func ProviderTest(c *gin.Context) {
+func ProviderTestHandler(c *gin.Context) {
 	id := c.Param("id")
 	_ = id
+	
 }

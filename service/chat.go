833c10e1e0219efc7b907ef916289fa933e9ec2b
hackerxiao <2945294768@qq.com>
2026-03-27 11:52:12 +0800

    feat: pop err加入traceID

--- service/chat.go
+++ service/chat.go
@@ -73,7 +73,7 @@ func BalanceChat(ctx context.Context, start time.Time, style string, before Befo
 			// 加权负载均衡
 			id, err := balancer.Pop()
 			if err != nil {
-				return nil, nil, err
+				return nil, nil, fmt.Errorf("balencer pop err: %v, traceID: %s", err, traceID)
 			}
 
 			modelWithProvider, ok := providersWithMeta.ModelWithProviderMap[id]
